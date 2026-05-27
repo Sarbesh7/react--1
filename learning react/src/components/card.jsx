4d205f9a4a925fe8eb1fa132f852c69b18ae9a31
Sarbesh Adhikari <sstudio985@gmail.com>
2026-05-27 23:17:12 +0545

    componennts

---
+++ learning react/src/components/card.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const card = () => {
+  return (
+    <>
+    <div className="Card">
+    <h1>HELLO WORLD</h1>
+    <h3>HELLO</h3>
+ </div>
+
+
+
+
+    </>
+  )
+}
+
+export default card
